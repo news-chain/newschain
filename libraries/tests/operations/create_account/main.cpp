fa49e24edaa9b4aaee69cb14bf90eb545e5015f1
OuYun <ouyun@yeah.net>
2018-07-11 19:33:11 +0800

    implement function - transfer

--- libraries/tests/operations/create_account/main.cpp
+++ libraries/tests/operations/create_account/main.cpp
@@ -17,16 +17,17 @@ int main(int argc, char **argv){
 
     try {
 
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 1; i++){
             std::thread([](){
-                http::client client("ws://192.168.2.180:8001");
+//                http::client client("ws://192.168.2.180:8001");
+                http::client client("ws://192.168.56.128:8001");
                 client.init();
 
                 auto start = fc::time_point::now();
 
                 auto ff =  factory::helper();
                 srand((unsigned)time(NULL));
-                for(int i = 1; i < 9000; i++){
+                for(int j = 1; j < 2; j++){
 
                     auto str = ff.create_account(NEWS_INIT_PRIVATE_KEY, 1, (account_name)(rand()));
                     std::string ret = string_json_rpc(fc::json::to_string(str));

096d32ca34d24040af7f47a11190e258e1a03432
OuYun <ouyun@yeah.net>
2018-07-12 16:49:12 +0800

    resolve conflicts after unintended merge operation.

--- libraries/tests/operations/create_account/main.cpp
+++ libraries/tests/operations/create_account/main.cpp
@@ -19,23 +19,15 @@ int main(int argc, char **argv){
 
         for(int i = 0; i < 1; i++){
             std::thread([](){
-<<<<<<< HEAD
 //                http::client client("ws://192.168.2.180:8001");
-                http::client client("ws://192.168.56.128:8001");
-=======
                 http::client client("ws://192.168.2.180:6002");
->>>>>>> 9bcf81ea63a851c70881930262477efcb393276c
                 client.init();
 
                 auto start = fc::time_point::now();
 
                 auto ff =  factory::helper();
                 srand((unsigned)time(NULL));
-<<<<<<< HEAD
                 for(int j = 1; j < 2; j++){
-=======
-                for(int i = 1; i < 100000; i++){
->>>>>>> 9bcf81ea63a851c70881930262477efcb393276c
 
                     auto str = ff.create_account(NEWS_INIT_PRIVATE_KEY, 1, (account_name)(rand()));
                     std::string ret = string_json_rpc(fc::json::to_string(str));

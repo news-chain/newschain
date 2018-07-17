5b5357ed0c4b43be2f12a1a98f8daabfa4c91217
OuYun <ouyun@yeah.net>
2018-07-17 13:55:06 +0800

    add accounts for ut

--- libraries/tests/operations/create_account/main.cpp
+++ libraries/tests/operations/create_account/main.cpp
@@ -20,28 +20,30 @@ int main(int argc, char **argv){
         for(int i = 0; i < 1; i++){
             std::thread([](){
 //                http::client client("ws://192.168.2.180:8001");
-                http::client client("ws://192.168.56.128:8001");
+                http::client client("ws://192.168.56.128:8002");
+                //http::client client("http://192.168.56.128:8001");
                 client.init();
 
                 auto start = fc::time_point::now();
 
                 auto ff =  factory::helper();
                 srand((unsigned)time(NULL));
-                for(int j = 1; j < 2; j++){
+                for(int j = 75; j < 76; j++){
 
-                    auto str = ff.create_account(NEWS_INIT_PRIVATE_KEY, 1, (account_name)(rand()));
+                    auto str = ff.create_account(NEWS_INIT_PRIVATE_KEY, 1, (account_name)j/*(rand())*/);
                     std::string ret = string_json_rpc(fc::json::to_string(str));
 //                    ddump((ret));
+                    dlog(ret);
                     client.send_message(ret);
                 }
                 auto end = fc::time_point::now();
                 ilog("time:${t}",("t", end - start));
                 client.start();
-            }).detach();
+            }).join();
 
         }
 
-        while(true);
+        //while(true);
 
 
 
